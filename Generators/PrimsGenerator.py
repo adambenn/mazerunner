a34faf02d6fad44b79ea765be8c83b9a430c70b1
Gabriel Bussieres <gab.bussieres@gmail.com>
2016-04-03 19:32:50 -0400

    Added Prims Generator

---
+++ Generators/PrimsGenerator.py
@@ -0,0 +1,41 @@
+import random
+from collections import deque
+from Generators.generator import *
+
+class PrimsGenerator(Generator):
+    def run(self, maze):
+        start = (0, random.randint(0, maze.size - 1))
+        end = (maze.size - 1, random.randint(0, maze.size - 1))
+
+        self.setEntranceExit(maze, start, end)
+
+        if self.debug:
+            print("Starting Prim's generation between cells {} and {}".format(start, end))
+
+        walls, visited = [], []
+
+        cell = maze.getCell(start)
+        visited.append(cell)
+
+        for neighbour in maze.getNeighbours(start):
+            walls.append((cell, neighbour))
+
+        while walls != []:
+            wall_index = random.randint(0, len(walls)-1)
+            (cell, neighbour) = walls[wall_index]
+
+            if neighbour not in visited:
+                cell.removeWallsBetweenCell(neighbour)
+                visited.append(neighbour)
+
+                for n in maze.getNeighbours(neighbour.coordinate):
+                    walls.append((neighbour, n))
+
+            walls.pop(wall_index)
+
+            if self.debug:
+                print("Removed walls between {} and {}".format(cell, neighbour))
+                print("Current maze:")
+                print(maze.graphicalRepresentation())
+
+        self.deleteRandomWalls(maze)
